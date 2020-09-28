cf179b0b82b9ba65494b1b151a64a743f5282ea6
kjl <uhiuuh@fghgf.hj>
2020-09-29 01:38:49 +0300

    add PHPDoc

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Services\VmService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
 
+    /**
+     * All VM logic
+     */
     private $vmService;
 
     public function __construct()
@@ -15,6 +19,11 @@ class ApiController extends Controller
         $this->vmService = new VmService();
     }
 
+    /**
+     * @api {get}
+     * @apiDescription gen index data
+     * @return JsonResponse
+     */
     public function index()
     {
         return response()->json([
@@ -23,6 +32,12 @@ class ApiController extends Controller
         ]);
     }
 
+    /**
+     * @api {post}
+     * @apiDescription pay drink
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function pay(Request $request)
     {
 
@@ -35,6 +50,12 @@ class ApiController extends Controller
 
     }
 
+    /**
+     * @api {post}
+     * @apiDescription buy drink
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function buy(Request $request){
 
         $result = $this->vmService->buy($request->all());
@@ -45,6 +66,11 @@ class ApiController extends Controller
         ]);
     }
 
+    /**
+     * @api {get}
+     * @apiDescription get change from VM
+     * @return JsonResponse
+     */
     public function getChange(){
 
         $result = $this->vmService->getChange();
@@ -55,6 +81,11 @@ class ApiController extends Controller
         ]);
     }
 
+    /**
+     * @api {get}
+     * @apiDescription reset to VM demo data
+     * @return JsonResponse
+     */
     public function resetDemoData(){
 
         $result = $this->vmService->resetDemoData();
